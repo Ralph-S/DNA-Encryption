dccc760fcd026120d182ca836812b26db7f0af3a
Ralph Sakhat <ralph.sakhat@lau.edu>
2024-04-16 11:09:47 +0300

    Fully functional backend integration (awaiting final frontend fix)

--- src/app.py
+++ src/app.py
@@ -3,14 +3,19 @@ from flask_cors import CORS
 import main
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/process-data',methods=['POST'])
 def process_data():
     data_input = request.json['plaintext']
     key_input = request.json['key']
     data = main.run(data_input,key_input)
-    data_to_send = ''.join(data)
-    return jsonify(data_to_send)
+    result_string = ''
+    for sublist in data:
+        for item in sublist:
+            for element in item:
+                result_string += element
+    return jsonify(result_string)
 
 if __name__ == '__main__':
     app.run(debug=True)
